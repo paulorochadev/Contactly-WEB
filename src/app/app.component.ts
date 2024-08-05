f333dfed70489f4a092a91f5cf966a0aa341f011
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-05 10:25:55 -0300

    feat: Cria endpoint getContacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Contactly-WEB';
+  
+  http = inject(HttpClient);
+
+  contacts$ = this.getContacts();
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7234/api/Contacts')
+  }
 }
