fb66e48e5e6cb51afc4a6d27f4f221faa3406ce2
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-05 12:18:06 -0300

    feat: Cria Formulário para criar um Contato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Contact } from '../models/contact.model';
@@ -8,7 +9,7 @@ import { Contact } from '../models/contact.model';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,7 +18,23 @@ export class AppComponent {
   
   http = inject(HttpClient);
 
+  contactsForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phone: new FormControl<string>(''),
+    favorite: new FormControl<boolean>(false)
+  })
+
   contacts$ = this.getContacts();
+
+  onFormSubmit(){
+    const addContactRequest = {
+      name: this.contactsForm.value.name,
+      email: this.contactsForm.value.email,
+      phone: this.contactsForm.value.phone,
+      favorite: this.contactsForm.value.favorite
+    }
+  }
 
   private getContacts(): Observable<Contact[]> {
     return this.http.get<Contact[]>('https://localhost:7234/api/Contacts')
